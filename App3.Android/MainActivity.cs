87347735e6c5c0b7ff49ddea9c87b6aaee91c47a
Sean Quinn <seanquinn@abas-usa.com>
2019-09-10 14:13:54 -0400

    commit reproduction project

---
+++ App3.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content.PM;
+
+namespace App3.Android
+{
+    [Activity(Label = "Test", Theme = "@style/AppTheme",  MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            LoadApplication(new App3.App());
+        }
+    }
+}
